2a14fbf274240d80a4c03b7f72bddb446ea3f518
Shreyas Sridhar <e0945817@u.nus.edu>
2024-09-03 14:32:45 +0800

    Changed links for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Section3_4 from './components/Section3_4';
 import Section5 from './components/Section5';
@@ -8,9 +8,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/section-3-4" element={<Section3_4 />} />
-        <Route path="/section-5" element={<Section5 />} />
+        <Route path="/scenetoaudio" element={<LandingPage />} />
+        <Route path="/scenetoaudio/section-3-4" element={<Section3_4 />} />
+        <Route path="/scenetoaudio/section-5" element={<Section5 />} />
       </Routes>
     </Router>
   );
